bde9bd0916456accdc4483b251c1193e31f03e41
MadaoDemo <1429544545@qq.com>
2019-02-18 20:59:50 +0800

    正在完成订单详情页面，页面代码已完成80%

--- src/components/center_part/OrderDetail.vue
+++ src/components/center_part/OrderDetail.vue
@@ -8,15 +8,86 @@
       </ul>
     </div>
     <div class="detail">
-      <div class="shop_img"></div>
-      <div>
-        <p>
-          <span>醉得意（杨家湾）</span>
-          <span>
+      <div class="shop-info">
+        <div class="shop-img"></div>
+        <div>
+          <p>
+            <span class="shop-title">醉得意（杨家湾）</span>
+            <span>
               <i class="iconfont">&#xeb9a;</i>
               收藏
-          </span>
-        </p>
+            </span>
+          </p>
+          <p>
+            <span class="order-no">订单号：2104080127576158444</span>
+            <span>商家电话：18162938906</span>
+          </p>
+        </div>
+      </div>
+      <div class="more-info">
+        <div class="shop-car">
+          <ul>
+            <li class="title">
+              <span>菜品</span>
+              <span>数量</span>
+              <span>小计（元）</span>
+            </li>
+            <li class="foods">
+              <p>
+                <span>得意糍粑鱼排</span>
+                <span>1</span>
+                <span>9.00</span>
+              </p>
+              <p>
+                <span>得意糍粑鱼排</span>
+                <span>1</span>
+                <span>9.00</span>
+              </p>
+            </li>
+            <li>
+              <span>配送费</span>
+              <span></span>
+              <span>6.10</span>
+            </li>
+            <li>
+              <p class="pay">
+                实际支付
+                <span>29.10</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="order-info">
+          <ul>
+            <li class="title">配送信息</li>
+            <li>
+              <p>
+                <span>送达时间：</span>
+                <span>尽快送出</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>联系人：</span>
+                <span>邓多多（先生）</span>
+              </p>
+              <p>
+                <span>联系电话：</span>
+                <span>15072772685</span>
+              </p>
+              <p>
+                <span>收货地址：</span>
+                <span>保利华16栋A栋</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>备注：</span>
+                <span>无备注</span>
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -27,6 +98,9 @@
   box-shadow: 0 0 5px #ccc;
   background: #fff;
   padding: 1.5625rem;
+}
+.order-detail ul {
+  padding: 0;
 }
 .order-detail .top {
   font-size: 1.125rem;
@@ -85,13 +159,104 @@
   margin-left: -46px;
   z-index: 1;
 }
-.detail{
-    display: flex;
-}
-.detail > .shop_img {
+.order-detail .detail .shop-img {
   width: 44px;
   height: 44px;
   background: url("http://127.0.0.1:5050/img/brand/miaomiao.jpg") no-repeat;
   background-size: cover;
+  margin-right: 2%;
+}
+.order-detail .detail .shop-info {
+  display: flex;
+}
+.order-detail ul {
+  list-style: none;
+}
+.order-detail p {
+  margin: 0;
+}
+.order-detail .detail {
+  background: #f8f8f8;
+  margin-top: 5%;
+  padding: 18px 0px 0px 30px;
+  box-shadow: 0 0 5px #ccc;
+}
+.shop-info {
+  font-size: 14px;
+  color: #999;
+  padding-right: 30px;
+}
+.shop-title {
+  font-size: 16px;
+  color: #333;
+}
+.shop-img + div {
+  width: 100%;
+}
+.shop-img + div > p:first-child {
+  display: flex;
+  justify-content: space-between;
+}
+.shop-img + div > p:first-child > span {
+  display: inline-block;
+}
+.order-no,
+.order-no + span {
+  display: inline-block;
+  width: 26%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  margin-right: 8%;
+}
+.order-detail .more-info {
+  display: flex;
+  border-top: 1px solid #eee;
+}
+.shop-car {
+  width: 55%;
+  font-size: 14px;
+  color: #666;
+}
+.shop-car ul li:not(:last-child) {
+  border-bottom: 1px solid #eee;
+}
+.shop-car ul li:not(:first-child) span {
+  margin-top: 3%;
+}
+.shop-car ul li span:first-child {
+  display: inline-block;
+  width: 50%;
+}
+.shop-car ul li span:nth-child(2) {
+  display: inline-block;
+  width: 10%;
+  text-align: center;
+}
+.shop-car ul li span:last-child {
+  display: inline-block;
+  width: 30%;
+  text-align: right;
+}
+.title {
+  font-weight: 700;
+  padding-top: 20px;
+  padding-bottom: 16px;
+}
+.order-info {
+  background: #fff;
+  width: 45%;
+  font-size: 14px;
+}
+.pay {
+  text-align: right;
+  padding-top: 3%;
+  padding-right: 7%;
+}
+.pay > span {
+  display: inline !important;
+  font-size: 1.625rem;
+  font-weight: 700;
+  color: #f74342;
 }
 </style>
